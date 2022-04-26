8a49be44ed05f9eca786ac82529b1c5d387150ea
Wm. Brett Henry <wbh1942@gmail.com>
2022-04-26 14:25:15 -0500

    Successfully merged. Menu GUI opens skeleton process. Need to allow for command line arguments to select pose.

--- Adityas_GUI/project/masterfully/MVVM/View/HomeView.xaml.cs
+++ Adityas_GUI/project/masterfully/MVVM/View/HomeView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,11 +29,103 @@ namespace Masterfully.MVVM.View
 
         public void Warrior1Click(object sender, RoutedEventArgs e)
         {
-            // Run Skeleton
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
             Process process = new Process();
             // Configure the process using the StartInfo properties.
-            process.StartInfo.FileName = "C:/Users/wbh19/Documents/Masterfully/Masterfully/x64/Debug/Masterfully.exe";
-            process.StartInfo.Arguments = "";
+            process.StartInfo.FileName = path+"/Skeleton.exe";
+            process.StartInfo.Arguments = "Warrior1";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+        }
+
+        public void Warrior2Click(object sender, RoutedEventArgs e)
+        {
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = path + "/Skeleton.exe";
+            process.StartInfo.Arguments = "Warrior2";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+        }
+
+        public void LotusClick(object sender, RoutedEventArgs e)
+        {
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = path + "/Skeleton.exe";
+            process.StartInfo.Arguments = "Lotus";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+        }
+
+        public void ExtendedTriangleClick(object sender, RoutedEventArgs e)
+        {
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = path + "/Skeleton.exe";
+            process.StartInfo.Arguments = "ExtendedTriangle";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+        }
+
+        public void LordOfDanceClick(object sender, RoutedEventArgs e)
+        {
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = path + "/Skeleton.exe";
+            process.StartInfo.Arguments = "LordOfDance";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+        }
+
+        public void SideStretchClick(object sender, RoutedEventArgs e)
+        {
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = path + "/Skeleton.exe";
+            process.StartInfo.Arguments = "SideStretch";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+        }
+
+        public void DownwardDogClick(object sender, RoutedEventArgs e)
+        {
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = path + "/Skeleton.exe";
+            process.StartInfo.Arguments = "DownwardDog";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+        }
+
+        public void UpwardDogClick(object sender, RoutedEventArgs e)
+        {
+            // Run Skeleton.
+            string path = Directory.GetCurrentDirectory();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = path + "/Skeleton.exe";
+            process.StartInfo.Arguments = "UpwardDog";
             process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
             process.Start();
             process.WaitForExit();// Waits here for the process to exit.
